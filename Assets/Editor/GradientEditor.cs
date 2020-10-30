18253de604189cd33575e7ff92c39b40ae9623ee
ColinCollins <crazypad@qq.com>
2020-10-30 13:41:44 +0800

    Add editor feature

--- Assets/Editor/GradientEditor.cs
+++ Assets/Editor/GradientEditor.cs
@@ -2,12 +2,153 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Rendering;
 
 public class GradientEditor : EditorWindow
 {
+	CustomGradient gradient;
+
+	const int borderSize = 10;
+	const float keyWidth = 10;
+	const float keyHeight = 20;
+
+	Rect gradientPreviewRect;
+	Rect[] keyRects;
+	bool mouseDownOverKey;
+	bool needsRepaint;
+	int selectedKeyIndex;
+
+
+	void draw() 
+	{
+		gradientPreviewRect = new Rect(borderSize, borderSize, position.width - borderSize * 2, 25);
+
+		GUI.DrawTexture(gradientPreviewRect, gradient.GetTexture((int)gradientPreviewRect.width));
+		keyRects = new Rect[gradient.NumKeys];
+
+		for (int i = 0; i < gradient.NumKeys; i++)
+		{
+			CustomGradient.ColorKey key = gradient.GetKey(i);
+			Rect keyRect = new Rect(gradientPreviewRect.x + gradientPreviewRect.width * key.Time - keyWidth / 2f,
+				gradientPreviewRect.yMax + borderSize, keyWidth, keyHeight
+				);
+
+			if (i == selectedKeyIndex)
+			{
+				EditorGUI.DrawRect(new Rect(keyRect.x - 2, keyRect.y - 2, keyRect.width + 4, keyRect.height + 4), Color.black);
+			}
+
+			EditorGUI.DrawRect(keyRect, key.Colour);
+
+			keyRects[i] = keyRect;
+		}
+
+		Rect settingRect = new Rect(borderSize, keyRects[0].yMax + borderSize, position.width - borderSize * 2, position.height);
+
+		GUILayout.BeginArea(settingRect);
+		// EditorGUI.DrawRect(settingRect, Color.white);
+		EditorGUI.BeginChangeCheck();
+		Color newColor = EditorGUILayout.ColorField(gradient.GetKey(selectedKeyIndex).Colour);
+		if (EditorGUI.EndChangeCheck()) 
+		{
+			gradient.UpdateKeyColour(selectedKeyIndex, newColor);
+		}
+
+		gradient.blendMode = (CustomGradient.BlendMode)EditorGUILayout.EnumPopup("Blend mode", gradient.blendMode);
+		gradient.randomizeColor = EditorGUILayout.Toggle("Randomize color", gradient.randomizeColor);
+		GUILayout.EndArea();
+	}
+
+	private void handleMouseListen() 
+	{
+		Event guiEvent = Event.current;
+
+		if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0)
+		{
+			for (int i = 0; i < keyRects.Length; i++)
+			{
+				if (keyRects[i].Contains(guiEvent.mousePosition))
+				{
+					mouseDownOverKey = true;
+					selectedKeyIndex = i;
+					needsRepaint = true;
+					break;
+				}
+			}
+
+			if (!mouseDownOverKey)
+			{
+				float keyTime = Mathf.InverseLerp(gradientPreviewRect.x, gradientPreviewRect.xMax, guiEvent.mousePosition.x);
+				Color interpolatedColor = gradient.Evaluate(keyTime);
+				Color randomColor = new Color(Random.value, Random.value, Random.value);
+				selectedKeyIndex = gradient.AddKey(gradient.randomizeColor ? randomColor : interpolatedColor, keyTime);
+				mouseDownOverKey = true;
+				needsRepaint = true;
+			}
+		}
+
+		if (guiEvent.type == EventType.MouseUp && guiEvent.button == 0)
+		{
+			OnLeftMouseUp();
+		}
+
+		if (guiEvent.type == EventType.MouseDrag && mouseDownOverKey && guiEvent.button == 0)
+		{
+			float keyTime = Mathf.InverseLerp(gradientPreviewRect.x, gradientPreviewRect.xMax, guiEvent.mousePosition.x);
+			selectedKeyIndex = gradient.UpdateKeyTime(selectedKeyIndex, keyTime);
+			needsRepaint = true;
+		}
+
+		if (guiEvent.keyCode == KeyCode.Space && guiEvent.type == EventType.KeyDown)
+		{
+			OnSpaceKeyDown();
+		}
+	}
+
+	private void OnLeftMouseUp() 
+	{
+		mouseDownOverKey = false;
+	}
+
+	private void OnSpaceKeyDown() 
+	{
+		gradient.RemoveKey(selectedKeyIndex);
+		if (selectedKeyIndex >= gradient.NumKeys)
+		{
+			selectedKeyIndex--;
+		}
+
+		needsRepaint = true;
+	}
+
+	public void SetGradient(CustomGradient gradient) 
+	{
+		this.gradient = gradient;
+	}
+
+	private void OnGUI()
+	{
+		draw();
+		handleMouseListen();
+
+		if (needsRepaint)
+		{
+			needsRepaint = false;
+			Repaint();
+		}
+	}
+
 	private void OnEnable()
 	{
 		titleContent.text = "Gradient Editor";
+		position.Set(position.x, position.y, 400, 150f);
+		minSize = new Vector2(200, 150);
+		maxSize = new Vector2(1920, 150);
+	}
+
+	private void OnDisable()
+	{
+		UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
 	}
 
 }
--- Assets/Scripts/Gradient/CustomGradient.cs
+++ Assets/Scripts/Gradient/CustomGradient.cs
@@ -1,21 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [System.Serializable]
 public class CustomGradient
 {
-	public Color Evaluate(float length) 
+	public enum BlendMode
 	{
-		return Color.Lerp(Color.white, Color.black, length);
+		Linear,
+		Discrete
+	}
+	public BlendMode blendMode;
+	public bool randomizeColor;
+
+	[SerializeField]
+	List<ColorKey> keys = new List<ColorKey>();
+
+	public CustomGradient() 
+	{
+		AddKey(Color.white, 0);
+		AddKey(Color.black, 1);
 	}
 
-	public Texture2D GetTexture(int width) 
+	public Color Evaluate(float time)
 	{
-		Texture2D texture = new Texture2D(width , 1);
+		ColorKey keyLeft = keys[0];
+		ColorKey keyRight = keys[keys.Count - 1];
+
+		for (int i = 0; i < keys.Count - 1; i++)
+		{
+			if (keys[i].Time <= time)
+			{
+				keyLeft = keys[i];
+			}
+
+			if (keys[i + 1].Time >= time) 
+			{
+				keyRight = keys[i + 1];
+				break;
+			}
+		}
+
+		if (blendMode == BlendMode.Linear)
+		{
+			float blendTime = Mathf.InverseLerp(keyLeft.Time, keyRight.Time, time);
+			return Color.Lerp(keyLeft.Colour, keyRight.Colour, blendTime);
+		}
+
+		return keyRight.Colour;
+	}
+
+	/// <summary>
+	/// Time is kind of color clip
+	/// </summary>
+	/// <param name="color"></param>
+	/// <param name="time"></param>
+	public int AddKey(Color color, float time)
+	{
+		ColorKey newKey = new ColorKey(color, time);
+		for (int i = 0; i < keys.Count; i++)
+		{
+			if (newKey.Time < keys[i].Time)
+			{
+				keys.Insert(i, newKey);
+				return i;
+			}
+		}
+
+		keys.Add(newKey);
+		return keys.Count - 1;
+	}
+
+	public int NumKeys
+	{
+		get
+		{
+			return keys.Count;
+		}
+	}
+
+	public ColorKey GetKey(int i)
+	{
+		return keys[i];
+	}
+
+	public Texture2D GetTexture(int width)
+	{
+		Texture2D texture = new Texture2D(width, 1);
 		Color[] colors = new Color[width];
 
-		for (int i = 0; i < width; i++) 
+		for (int i = 0; i < width; i++)
 		{
 			colors[i] = Evaluate((float)i / (width - 1));
 		}
@@ -23,5 +98,59 @@ public class CustomGradient
 		texture.SetPixels(colors);
 		texture.Apply();
 		return texture;
+	}
+
+	public int UpdateKeyTime(int index, float time)
+	{
+		Color color = keys[index].Colour;
+		RemoveKey(index);
+		return AddKey(color, time);
+	}
+
+	public void UpdateKeyColour(int index, Color color)
+	{
+		keys[index] = new ColorKey(color, keys[index].Time);
+	}
+
+	public void RemoveKey(int index) 
+	{
+		if (keys.Count >= 2) 
+		{
+			keys.RemoveAt(index);
+		}
+	}
+
+	[System.Serializable]
+	public struct ColorKey 
+	{
+		/// <summary>
+		/// SerializeField 强制序列化一个私有变量
+		/// </summary>
+		[SerializeField]
+		Color color;
+		[SerializeField]
+		float time;
+
+		public ColorKey(Color _color, float _time) 
+		{
+			color = _color;
+			time = _time;
+		}
+
+		public Color Colour 
+		{
+			get 
+			{
+				return color;
+			}
+		}
+
+		public float Time 
+		{
+			get 
+			{
+				return time;
+			}
+		}
 	}
 }
